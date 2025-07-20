cc239d3d5046bbf0ee64d609b3e879e7c06fa3dd
SolisysDigital <solisys@solisyspro.com>
2025-07-19 23:00:06 -0500

    ts to js

--- server/routes.ts
+++ server/routes.ts
@@ -1,6 +1,6 @@
 import type { Express } from "express";
 import { createServer, type Server } from "http";
-import { storage } from "./storage.ts";
+import { storage } from "./storage.js";
 import { insertClientSchema } from "@shared/schema";
 import { z } from "zod";
 
